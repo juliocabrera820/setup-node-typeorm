e06147822b2524359cd3251bb1710a5190c0b867
juliocabrera820 <juliocabrera820@gmail.com>
2020-08-17 10:55:12 -0500

    chore: babel config

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  plugins: [['module-resolver', {
+    alias: {
+      '@controllers': './src/controllers',
+      '@models': './src/models',
+      '@config': './src/config',
+      '@views': './src/views'
+    }
+  }]],
+  ignore: ['**/*.spec.ts']
+}
