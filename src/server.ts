a0dd3962ff16e63b87dfa205a649ffacc5434836
juliocabrera820 <juliocabrera820@gmail.com>
2020-08-18 08:04:19 -0500

    fix: use environment variable

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import { createConnection } from 'typeorm'
 dotenv.config()
 createConnection()
 
-const PORT = 4000
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
 })
