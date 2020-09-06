451e4e8baacebcef1c6ae62f33a95b546c0e7713
juliocabrera820 <juliocabrera820@gmail.com>
2020-09-06 14:07:00 -0500

    feat(cors): use cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { json } from 'express'
 import { createConnection } from 'typeorm'
+import cors from 'cors'
 
 createConnection()
 const app = express()
+
+app.use(cors())
+app.use(json())
 
 export default app
